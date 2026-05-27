ba598b99099af8dd9a4e7afc02a908f70ee936c7
cassomaa927-sudo <cassomaa927@gmail.com>
2026-05-27 14:03:40 +0100

    Delete page.tsx

--- page.tsx
+++
@@ -1,9 +0,0 @@
-import PaymentForm from "@/components/PaymentForm";
-
-export default function Home() {
-  return (
-    <div className="flex justify-center items-center min-h-[calc(100vh-6rem)]">
-      <PaymentForm />
-    </div>
-  );
-}
