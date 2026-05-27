91c0224c1e21673d70c06ac0cd8c3da33833f669
cassomaa927-sudo <cassomaa927@gmail.com>
2026-05-27 14:15:07 +0100

    Create page.tsx

---
+++ app/pagamento/page.tsx
@@ -0,0 +1,9 @@
+import PaymentForm from "@/components/PaymentForm";
+
+export default function PagamentoPage() {
+  return (
+    <div className="flex justify-center items-center min-h-screen bg-deepCharcoal">
+      <PaymentForm />
+    </div>
+  );
+}
