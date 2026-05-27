491ec06177410b6f875e687e02548aecee9afda8
cassomaa927-sudo <cassomaa927@gmail.com>
2026-05-27 13:43:10 +0100

    Create page.tsx

---
+++ page.tsx
@@ -0,0 +1,9 @@
+import PaymentForm from "@/components/PaymentForm";
+
+export default function Home() {
+  return (
+    <div className="flex justify-center items-center min-h-[calc(100vh-6rem)]">
+      <PaymentForm />
+    </div>
+  );
+}
